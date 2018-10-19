63aa54252cd2002b4a5ee5ad5963baec84f45f56
ronweasleyding <ronweasleyding@163.com>
2018-10-19 18:43:44 +0800

    20181019

---
+++ java/Chapter 3/3.1/SequentialSearchSignTable.java
@@ -0,0 +1,48 @@
+import edu.princeton.cs.algs4.StdOut;
+
+public class SequentialSearchSignTable<Key, Value> {
+    private class Node {
+        Key key;
+        Value val;
+        Node next;
+
+        public Node(Key key, Value val, Node next) {
+            this.key = key;
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    private Node first;
+    private int num = 0;
+    public Value get(Key key) {
+        for (Node x = first; x != null; x = x.next) {
+            if (key.equals(x.key)) return x.val;
+        }
+        return null;
+
+    }
+
+    public void put(Key key, Value val) {
+        for (Node x = first; x != null; x = x.next) {
+            if (key.equals(x.key)) {
+                x.val = val;
+                return;
+            }
+        }
+        first = new Node(key, val, first);
+        num += 1;
+
+    }
+
+
+    public static void main(String[] args) {
+        SequentialSearchSignTable<String, Integer> dic = new SequentialSearchSignTable<String, Integer>();
+        dic.put("2", 2);
+        dic.put("1", 1);
+        StdOut.println(dic.get("1"));
+
+
+    }
+
+}

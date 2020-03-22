b813a4cb150b7e67bea159be442970b7b40cf8b2
ron <ronweasleyding@163.com>
2020-03-22 14:15:04 +0800

    Stack2

---
+++ leetcode/数据结构/栈后入先出的数据结构/20.Valid Parentheses.有效的括号.py
@@ -0,0 +1,27 @@
+class Solution:
+	def isValid(self, s: str) -> bool:
+		if s == '':
+			return True
+		stack = []
+		dic = {
+			'(': ')',
+			'[': ']',
+			'{': '}'
+		}
+
+		for c in s:
+			if c in dic.keys():
+				stack.append(c)
+			elif c in dic.values():
+				if stack:
+					if c != dic[stack.pop()]:
+						return False
+				else:
+					return False
+		return not stack
+
+
+if __name__ == '__main__':
+	a = Solution()
+	b = a.isValid("([]){}")
+	print(b)

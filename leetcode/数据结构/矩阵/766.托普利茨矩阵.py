91c4a84631f7c93986391794d9cbc861c065f6c6
RonDingDing <ronweasleyding@163.com>
2025-04-16 23:33:39 +0800

    20250416

--- leetcode/数据结构/矩阵/766.托普利茨矩阵.py
+++ leetcode/数据结构/矩阵/766.托普利茨矩阵.py
@@ -47,5 +47,15 @@ class Solution:
         return True
 
 
+class Solution2:
+    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+        m, n = len(matrix), len(matrix[0])
+        for i in range(1, m):
+            for j in range(1, n):
+                if matrix[i][j] != matrix[i - 1][j - 1]:
+                    return False
+        return True
+
+
 matrix = [[1, 2, 3, 4], [5, 1, 2, 3], [9, 5, 1, 2]]
 print(Solution().isToeplitzMatrix(matrix))
